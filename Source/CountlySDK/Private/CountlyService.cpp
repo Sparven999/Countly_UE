2780d3b0a39c3d2eb5d8cc278106e133c3c8dfc4
Oscar Hvarfvén <oskis_999@hotmail.com>
2020-04-13 22:33:59 +0200

    Added init-function to CountlyService
    Added Initialize() to CountlyService, this will be called by the user to Init the service. Without proper init the service won't start.

--- Source/CountlySDK/Private/CountlyService.cpp
+++ Source/CountlySDK/Private/CountlyService.cpp
@@ -3,3 +3,27 @@
 
 #include "CountlyService.h"
 
+UCountlyService::UCountlyService(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+	/*____________________________________________________________________________________________________________
+	 *
+	 * Initialize and set default values for all properties.
+	 * _____________________________________________________________________________________________________________*/
+	
+	URL.Empty();
+	AppKey.Empty();
+	DeviceID.Empty();
+}
+
+bool UCountlyService::Initialize()
+{
+	bool result = false;
+	
+	if(URL.IsEmpty() && AppKey.IsEmpty() && DeviceID.IsEmpty())
+	{
+		
+	}
+
+	return result;
+}
--- Source/CountlySDK/Public/CountlyService.h
+++ Source/CountlySDK/Public/CountlyService.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "Containers/Queue.h"
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
 #include "CountlyService.generated.h"
@@ -13,5 +14,24 @@ UCLASS()
 class COUNTLYSDK_API UCountlyService : public UObject
 {
 	GENERATED_BODY()
+
+
+public:
+
+	UCountlyService(const FObjectInitializer& ObjectInitializer);
 	
+	UPROPERTY()
+	FString URL;
+
+	UPROPERTY()
+	FString AppKey;
+
+	UPROPERTY()
+	FString DeviceID;
+
+	UFUNCTION()
+	bool Initialize();
+
+private:
+	TQueue<bool, EQueueMode::Mpsc> RequestQueue;		//We want this to be of type MPSC so we can use it easier with another thread. This to not bog down the game thread! 
 };
